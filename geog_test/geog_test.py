3003cd0cd80c716556b5739d59eb2fedf4abfa90
Yinan Feng <53467081+1041267458@users.noreply.github.com>
2023-03-28 10:43:10 -0400

    add password generater

--- geog_test/geog_test.py
+++ geog_test/geog_test.py
@@ -1,1 +1,20 @@
 """Main module."""
+
+import random
+import string
+
+def get_random_string(length=8):
+    # choose from all lowercase letter, uppercase, numbers. (optional: punctuation)
+    letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    print("Random string of length", length, "is:", result_str)
+
+while True:
+    try:
+     ps_length = input("enter the length of password you want: (enter 'quit' to quit)")
+    except ValueError:
+        print('please enter a vaild number.')
+    if ps_length == 'quit':
+        break
+    else:
+     get_random_string(int(ps_length))
